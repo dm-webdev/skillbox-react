bf63035fe8f914a5aa02555fa4c9550607bf7416
dm-webdev <denis.mironenko@mail.ru>
2020-09-09 23:56:59 +0300

    Client webpack config

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export function Header() {
+  return (
+    <header>
+      <h1>React App</h1>
+      <p style={{color: "red"}}>Hello world</p>
+      <p style={{color: "blue"}}>Hello world</p>
+    </header>
+  );
+}
